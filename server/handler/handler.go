9434556ae1efb7961bead59c9c841d40124bace1
gericass <bonjovi1926@yahoo.co.jp>
2018-05-25 14:09:32 +0900

    add: apply gotify

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -1,6 +1,29 @@
 package handler
 
-import "github.com/labstack/echo"
+import (
+	"github.com/labstack/echo"
+	"github.com/gericass/gotify"
+	"net/http"
+)
+
+var Auth gotify.OAuth
+var Token gotify.Gotify
+
+// Handler : Controller for https://localhost:3000/
+func AuthHandler(c echo.Context) error {
+	url := Auth.AuthURL() // Get the Redirect URL for authenticate
+	return c.Redirect(301, url)
+}
+
+// CallbackHandler : Controller for https://localhost:3000/callback/
+func CallbackHandler(c echo.Context) error {
+	t, err := Auth.GetToken(c.Request()) // Get the token for using Spotify API
+	if err != nil {
+		return err
+	}
+	Token = t
+	return c.String(http.StatusOK, "Authentication success")
+}
 
 func PlaylistHandler(c echo.Context) error {
 	return nil
--- server/main.go
+++ server/main.go
@@ -3,9 +3,40 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/gericass/feel-music-mock/server/handler"
+
+	"github.com/gericass/feel-music-mock/server/db"
+	"github.com/labstack/echo/middleware"
+	"github.com/gericass/gotify"
 )
+
+func dbMiddleware(h echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cnn, err := db.ConnectDB()
+		if err != nil {
+			return err
+		}
+		defer cnn.Close()
+		cc := &handler.CustomContext{c, cnn}
+		return h(cc)
+	}
+}
+
+const clientID = ""
+const clientSecret = ""
+const callbackURI = "https://localhost:5000/callback/"
 
 func main() {
 	e := echo.New()
+	e.Use(dbMiddleware)
+	e.Pre(middleware.HTTPSRedirect())
+
+	handler.Auth = gotify.Set(clientID, clientSecret, callbackURI)
+
+	e.Use(middleware.Logger())
+
 	e.GET("/playlist", handler.PlaylistHandler)
+	e.GET("/auth", handler.AuthHandler)
+	e.GET("/callback/", handler.CallbackHandler)
+
+	e.Start(":5000")
 }
