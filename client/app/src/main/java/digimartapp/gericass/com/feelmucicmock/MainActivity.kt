20c8b1d036f1700d91dfef6d34f068c1d9022e23
gericass <bonjovi1926@yahoo.co.jp>
2018-05-27 00:20:42 +0900

    fix: rename home xml

--- client/app/src/main/java/digimartapp/gericass/com/feelmucicmock/MainActivity.kt
+++ client/app/src/main/java/digimartapp/gericass/com/feelmucicmock/MainActivity.kt
@@ -10,7 +10,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 
 class MainActivity : AppCompatActivity(), MainNavigator, NavigationView.OnNavigationItemSelectedListener {
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity(), MainNavigator, NavigationView.OnNaviga
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         val mainFragment = MainFragment()
